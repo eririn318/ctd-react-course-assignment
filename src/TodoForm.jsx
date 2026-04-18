2c809f2a5fa9105926d62a3c31467763c9314c10
eririn318git config --global user.email eririn75@hotmail.com <eririn75@hotmail.com>
2026-04-17 17:54:29 -0700

    complete lesson-04

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,9 +1,31 @@
-function TodoForm() {
+import {useRef} from "react"
+
+function TodoForm({onAddTodo}) {
+    const inputRef = useRef();
+
+    function handleAddTodo(event) {
+     
+        event.preventDefault();
+
+
+    // console.log("Event object:", event);
+    // console.log("Event target:", event.target);
+    // console.log("Input value:", event.target.todoTitle.value);
+
+    const todoTitle = event.target.todoTitle.value.trim()
+
+    if(todoTitle){
+      onAddTodo(todoTitle)  
+      event.target.reset(); // clears input
+      inputRef.current.focus(); // focuses input
+    }
+}
+
     return(
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Todo</label>
-            <input type="text" id="todoTitle" />
-            <button type="submit" disabled>
+            <input ref={inputRef} type="text" name ="todoTitle" id="todoTitle" placeholder={"todo text"} required/>
+            <button  type="submit" >
                 Add Todo
             </button>
         </form>
