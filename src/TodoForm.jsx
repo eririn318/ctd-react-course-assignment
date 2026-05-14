90699e1ae37f00485779f6c9655e0b9516d1abe6
eririn318git config --global user.email eririn75@hotmail.com <eririn75@hotmail.com>
2026-05-14 01:27:21 -0700

    complete lesson-05

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,26 +1,39 @@
-import {useRef} from "react"
-
+// import {useRef, useState} from "react"
+import {useState} from "react"
 function TodoForm({onAddTodo}) {
-    const inputRef = useRef();
+    // const inputRef = useRef();
+    const [workingTodoTitle, setWorkingTodoTitle] = useState("")
+
 
     function handleAddTodo(event) {
      
         event.preventDefault();
 
+    // const todoTitle = event.target.todoTitle.value.trim()
 
-    const todoTitle = event.target.todoTitle.value.trim()
-
-    if(todoTitle){
-      onAddTodo(todoTitle)  
-      event.target.reset(); // clears input
-      inputRef.current.focus(); // focuses input
+    if(workingTodoTitle){
+      onAddTodo(workingTodoTitle)  
+    //   event.target.reset(); // clears input
+      setWorkingTodoTitle("")
+    //   inputRef.current.focus(); // focuses input
     }
 }
     return(
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Todo</label>
-            <input ref={inputRef} type="text" name ="todoTitle" id="todoTitle" placeholder={"Todo text"} required/>
-            <button  type="submit" >
+            <input 
+            // ref={inputRef} 
+            type="text" 
+            name ="todoTitle" 
+            id="todoTitle" 
+            placeholder={"Todo text"} 
+            value={workingTodoTitle} //// controlled — React controls what shows--displays state in input/value ties the input box to React state — whatever is in state is what shows in the input box. This is called a controlled component. 
+            onChange={(event) => setWorkingTodoTitle(event.target.value)} //updates state on user's input type
+            required/>
+            <button  
+            type="submit" 
+            disabled={!workingTodoTitle.trim()}
+            >
                 Add Todo
             </button>
         </form>
